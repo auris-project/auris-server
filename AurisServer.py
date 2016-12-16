11ad449da636855a6c38628cf696e0d68ab81d22
Luzenildo Junior <luzenildojunior@lavid.ufpb.br>
2016-12-16 10:26:11 -0300

    Server Modified and front-end integration #3

--- AurisServer.py
+++ AurisServer.py
@@ -2,24 +2,23 @@
 # -*- coding: utf-8 -*-
 
 #Imports:
-from flask import Flask, request
+from flask import Flask, request, render_template, Response, jsonify, send_from_directory
 from subprocess import Popen
 import os
 import socket
+import json
 
 #Path file Configurations
 path1 = os.environ.get('AURIS_HOME_PATH') #Get Auris Home filepath.
 path2 = os.environ.get('AURIS_FILES') #Get Auris Files filepath
 
 #Flask HTML Configurations:
-app = Flask(__name__, static_url_path='', static_folder='html')
+app = Flask(__name__, static_url_path='', static_folder='front', template_folder="front/templates")
 app.config['UPLOAD_FOLDER_MUSIC'] = "%s/audios" %(path2) #path to save uploaded songs.
 app.config['UPLOAD_FOLDER_AURIS_FILES'] = "%s/auris_melodies" %(path2) #path to save uploaded songs.
 app.config['ALLOWED_EXTENSIONS'] = set(['wav', 'mp3', 'txt']) #Extensions supported by Auris Midi Melody Generator.
 
 #Server Socket Configurations:
-ip = '192.168.0.105' #IP to connect Arduino through Socket.
-port = 3300 #Port to send data to Arduino through Socket, Arduino must listen in this port.
 buffersize = 1024 #Buffer size to receive and send messages.
 
 #Arduino Communication Flags:
@@ -27,6 +26,10 @@ message1 = "write" #Arduino Message 1: This message throws a flag to Arduino wri
 message2 = "start" #Arduino Message 2: This message throws a flag to Arduino play Auris files.
 message3 = "stop"  #Arduino Message 3: This message throws a flag to Arduino stop execution of Auris files.
 ponto_de_parada = "*" #End of file. This message notify the end of file to Arduino stop write file into SD Card.
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 '''
 # Route to Generate Midi Melodies using the Auris Controller Midi-Melody Generator Module.
@@ -40,7 +43,8 @@ def generate_midi(music):
 	path = "/.%s/auris-controller/auris_controller.out" %(path1) #Auris Controller Midi-Melody Generator Module path.
 	#Create Process to execute Midi-Melody Generator Module.
 	process = Popen([path, "midiMelody", music]) #System call passing "midiMelody" and music name parameters.
-	return "Midi Generated!", 200 #In case of success, this message should be displayed in your Web Browser.
+	process.wait()
+	return Response(status=200) #In case of success, this message should be displayed in your Web Browser.
 
 '''
 # Route to Generate Auris Melodies Files using the Auris Controller Midi-Melody Generator Module.
@@ -55,7 +59,20 @@ def generate_auris(music):
 	path = "/.%s/auris-controller/auris_controller.out" %(path1) #Auris Controller Midi-Melody Generator Module path.	
 	#Create Process to execute Midi-Melody Generator Module.
 	process = Popen([path, "aurisStream", music]) #System call passing "aurisStream" and music name parameters.
-	return "Auris File Generated!", 200 #In case of success, this message should be displayed in your Web Browser.
+	process.wait()
+	return Response(status=200) #In case of success, this message should be displayed in your Web Browser.
+
+@app.route("/api/download-auris/<music>", methods=['GET'])
+def download_auris(music):
+	music_path = "%s/auris_melodies/" %(path2) #Song file path
+	filename = "%s.txt" %(music) #Song name
+	return send_from_directory(directory=music_path, filename=filename, as_attachment=True)
+
+@app.route("/api/play-music/<music>", methods=['GET'])
+def play_song(music):
+	music_path = "%s/audios/" %(path2) #Song file path
+	filename = "%s.wav" %(music) #Song name
+	return send_from_directory(directory=music_path, filename=filename)
 
 '''
 # Route to send Auris Melodies files to Arduino.
@@ -64,11 +81,16 @@ def generate_auris(music):
 # PS1: Before send Auris Melody File to Arduino, you MUST fist GENERATE Midi-Melody Files using /api/generate-midi/MUSIC_NAME ...
 # and GENERATE Auris-Melody Files using /api/generate-auris/MUSIC_NAME
 '''
-@app.route("/api/arduino-post/<music>", methods=['GET'])
-def post_arduino(music):
+@app.route("/api/arduino-post/<ip>/<port>/<music>", methods=['GET'])
+def post_arduino(ip, port, music):
 	path = "%s/auris_melodies/%s.txt" %(path2,music) #Concatenate filepath with song name received through URL Parameter.
 	work_file = open(path, "rb") #Open Auris File located in "path" string variable.
 	file_size = str(os.stat(path).st_size) #Get File size.
+	port = int(port)
+
+	print "Connecting ip: ", ip
+	print "At port: ", port
+	print "Music: ", music
 
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Create Socket.
 	s.connect((ip, port)) #Connect in Arduino server.
@@ -92,7 +114,7 @@ def post_arduino(music):
 
 	print "Message Received from Arduino: ", data #Print message received from Arduino
 	s.close #Close Socket
-	return "Post Sent!", 200 #This message should be displayed in your Web Browser.
+	return Response(status=200) #This message should be displayed in your Web Browser.
 
 '''
 # Route to send Start flag message to Arduino.
@@ -101,33 +123,44 @@ def post_arduino(music):
 # Example: http://Your_IP:Port/api/start/SONG_NAME.
 # If you have installed all dependencies using our script installer, the song will be played in your computer using PUREDATA.
 '''
-@app.route("/api/start/<music>", methods=['GET'])
-def start(music):
+@app.route("/api/start/<ip>/<port>/<music>", methods=['GET'])
+def start(ip, port, music):
+	port = int(port)
+
+	print "Connecting ip: ", ip
+	print "At port: ", port
+	print "Music: ", music
+
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Create Socket to send message.
 	s.connect((ip, port)) #Connect in Arduino Socket Server.
 	print "Sending Start to Arduino"
 	s.send(message2) #Send Start Message Flag to Arduino.
 	print "Start Sent"
 	s.close #Close Socket.
-	path = "/.%s/auris-core/auris-filter/src/main" %(path1) #Auris-Filter to play audio path.	
-	music_path = "%s/audios/%s.wav" %(path2, music) #Song file path
+	#path = "/.%s/auris-core/auris-filter/src/main" %(path1) #Auris-Filter to play audio path.	
+	#music_path = "%s/audios/%s.wav" %(path2, music) #Song file path
 	#Create Process to play music.
-	process = Popen([path, "1", music_path]) #System call sending "1" and music name as arguments.
-	return "Started!", 200 #In case of success, this message should be displayed in your Web Browser.
+	#process = Popen([path, "1", music_path]) #System call sending "1" and music name as arguments.
+	return Response(status=200) #In case of success, this message should be displayed in your Web Browser.
 
 '''
 # Route to send Stop flag message to Arduino.
 # To send this message, you first MUST first Start Arduino execution using api/start route.
 '''
-@app.route("/api/stop", methods=['GET'])
-def stop():
+@app.route("/api/stop/<ip>/<port>", methods=['GET'])
+def stop(ip, port):
+	port = int(port)
+
+	print "Connecting ip: ", ip
+	print "At port: ", port
+
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Create Socket to send message.
 	s.connect((ip, port)) #Connect in Arduino Socket Server.
 	print "Sending Stop to Arduino"
 	s.send(message3) #Send Start Message Flag to Arduino.
 	print "Stop sent"
 	s.close #Close Socket.
-	return "Stopped!", 200 #In case of success, this message should be displayed in your Web Browser.
+	return Response(status=200) #In case of success, this message should be displayed in your Web Browser.
 
 '''
 # Method to verify if the uploaded file is supported by Auris Melody Generator.
@@ -135,13 +168,6 @@ def stop():
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
-
-'''
-# Route to show upload form.
-'''
-@app.route('/upload')
-def index():
-    return app.send_static_file('index.html') #Show the default upload index.html page
 
 '''
 # This route handles uploaded files and check if the extension is supported.
@@ -162,9 +188,9 @@ def upload():
         elif filename.find(".txt") > 0:
         	file.save(os.path.join(app.config['UPLOAD_FOLDER_AURIS_FILES'], filename))
         # Redirect the user to the uploaded_file route, which will basicaly show on the browser the uploaded file
-        return "File Uploaded!", 200 #In case of success, this message should be displayed in your Web Browser.
-    return "File cannot be uploaded", 405
+        response=Response(status=200)
+        return response
 
 #Python and Flask Configurations:
 if __name__ == "__main__":
-	app.run(host="127.0.0.1", port=5000, debug=True) #IP and Port use to send HTML Requests.
+	app.run(host="127.0.0.1", port=5300, debug=True, threaded=True) #IP and Port use to send HTML Requests.
